905206cb1048073b5b308488f6a8909c417f11b8
info-raphael <info-raphael@raphael-desktop.(none)>
2009-06-29 11:24:04 -0300

    Layout Intranet e Home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def f_data(d)
+      "#{d.to_s[8..9]}/#{d.to_s[5..6]}/#{d.to_s[0..3]}"
+  end
 end
