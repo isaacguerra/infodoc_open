0019c43bc2e1c630d19c2637c8d801153e7fae6b
info-raphael <info-raphael@raphael-desktop.(none)>
2009-09-17 08:38:48 -0300

    correcoes na estilizacao do status do usuario

--- config/environment.rb
+++ config/environment.rb
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
    config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
    config.gem 'brazilian-rails'
    #require 'brazilian-rails'
-   #require 'brI18n'
+   require 'brI18n'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
