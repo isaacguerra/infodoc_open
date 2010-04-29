093fb347fa934665a5fee9c7e7a76006e0636677
raphael <raphael@raphael-desktop.(none)>
2010-04-29 11:18:58 -0300

    organizacao das ferramentas de cadastro e melhor performace no plugin de navegacao de arquivos

--- public/javascripts/treeView.js
+++ public/javascripts/treeView.js
@@ -1,15 +1,8 @@
-// second example
+// aaa
 	$("#navegacao_arquivos").treeview({
-			toggle: function() {
-				console.log("%s was toggled.", $(this).find(">span").text());
-			},
-			animated: "fast",
-		collapsed: true,
-		unique: true,
-		persist: "cookie",
-		toggle: function() {
-			window.console && console.log("%o was toggled", this);
-		}
+			
+		collapsed: true
+		
 		});
 	
 
